eth-ravenxcb54/charon

commit f55f1d1b292206888f7ae0180c995db9871d68e6

app/log: align warn and error APIs (#468)
Aligns `log.Warn` and `log.Error` APIs. Since warn and error are mostly related to logging errors, just at different level, the API should be the same. Using `z.Err(err)` also resulted in inconsistent logging, which is now mitigated by aligned APIs. Also support `nil` errors for both `log.Warn` and `log.Error` that just does logging.
 
category: refactor
ticket: none
feature_set: stable

app/log/log.go
@@ -88,18 +88,39 @@ func Info(ctx context.Context, msg string, fields ...z.Field) {
 	logger.Info(msg, zfl...)
 }
 
-// Warn logs the message and fields (incl fields in the context) at Warn level.
+// Warn wraps err with msg and fields and logs it (incl fields in the context) at Warn level.
+// Nil err is supported and results in similar behaviour to Info, just at Warn level.
 // Warn should only be used when a problem is encountered that *does not* require any action to be taken.
-func Warn(ctx context.Context, msg string, fields ...z.Field) {
-	zfl := unwrapDedup(ctx, fields...)
-	trace.SpanFromContext(ctx).AddEvent("log.Warn: "+msg, toAttributes(zfl))
-	logger.Warn(msg, zfl...)
-	incWarnCounter(ctx)
+func Warn(ctx context.Context, msg string, err error, fields ...z.Field) {
+	if err == nil {
+		zfl := unwrapDedup(ctx, fields...)
+		trace.SpanFromContext(ctx).AddEvent("log.Warn: "+msg, toAttributes(zfl))
+		logger.Warn(msg, zfl...)
+		incErrorCounter(ctx)
+
+		return
+	}
+
+	err = errors.SkipWrap(err, msg, 2, fields...)
+	zfl := unwrapDedup(ctx, errFields(err))
+	trace.SpanFromContext(ctx).RecordError(err, trace.WithStackTrace(true), toAttributes(zfl))
+	logger.Warn(err.Error(), zfl...)
+	incErrorCounter(ctx)
 }
 
 // Error wraps err with msg and fields and logs it (incl fields in the context) at Error level.
+// Nil err is supported and results in similar behaviour to Info, just at Error level.
 // Error should only be used when a problem is encountered that *does* require action to be taken.
 func Error(ctx context.Context, msg string, err error, fields ...z.Field) {
+	if err == nil {
+		zfl := unwrapDedup(ctx, fields...)
+		trace.SpanFromContext(ctx).AddEvent("log.Error: "+msg, toAttributes(zfl))
+		logger.Error(msg, zfl...)
+		incErrorCounter(ctx)
+
+		return
+	}
+
 	err = errors.SkipWrap(err, msg, 2, fields...)
 	zfl := unwrapDedup(ctx, errFields(err))
 	trace.SpanFromContext(ctx).RecordError(err, trace.WithStackTrace(true), toAttributes(zfl))

app/app.go
@@ -496,7 +496,7 @@ func wireValidatorMock(conf Config, pubshares []eth2p0.BLSPubKey, sched core.Sch
 				eth2http.WithTimeout(time.Second*10), // Allow sufficient time to block while fetching duties.
 			)
 			if err != nil {
-				log.Warn(ctx, "Cannot connect to validatorapi", z.Err(err))
+				log.Warn(ctx, "Cannot connect to validatorapi", err)
 				return
 			}
 
@@ -515,18 +515,18 @@ func callValidatorMock(ctx context.Context, duty core.Duty, cl eth2client.Servic
 	case core.DutyAttester:
 		err := validatormock.Attest(ctx, cl.(*eth2http.Service), signer, eth2p0.Slot(duty.Slot), pubshares...)
 		if err != nil {
-			log.Warn(ctx, "Attestation failed", z.Err(err))
+			log.Warn(ctx, "Attestation failed", err)
 		} else {
 			log.Info(ctx, "Attestation success", z.I64("slot", duty.Slot))
 		}
 	case core.DutyProposer:
 		err := validatormock.ProposeBlock(ctx, cl.(*eth2http.Service), signer, eth2p0.Slot(duty.Slot), addr, pubshares...)
 		if err != nil {
-			log.Warn(ctx, "Failed to propose block", z.Err(err))
+			log.Warn(ctx, "Failed to propose block", err)
 		} else {
 			log.Info(ctx, "Block proposed successfully", z.I64("slot", duty.Slot))
 		}
 	default:
-		log.Warn(ctx, "Invalid duty type")
+		log.Warn(ctx, "Invalid duty type", nil)
 	}
 }

app/featureset/config.go
@@ -72,7 +72,7 @@ func Init(ctx context.Context, config Config) error {
 			}
 		}
 		if !ok {
-			log.Warn(ctx, "Ignoring unknown enabled feature", z.Str("feature", f))
+			log.Warn(ctx, "Ignoring unknown enabled feature", nil, z.Str("feature", f))
 		}
 	}
 
@@ -85,7 +85,7 @@ func Init(ctx context.Context, config Config) error {
 			}
 		}
 		if !ok {
-			log.Warn(ctx, "Ignoring unknown disabled feature", z.Str("feature", f))
+			log.Warn(ctx, "Ignoring unknown disabled feature", nil, z.Str("feature", f))
 		}
 	}
 

app/log/log_test.go
@@ -44,8 +44,8 @@ func TestWithContext(t *testing.T) {
 
 	log.Debug(ctx1, "msg1", z.Int("ctx1", 1))
 	log.Info(ctx2, "msg2", z.Int("ctx2", 2))
-	log.Warn(ctx3a, "msg3a")
-	log.Warn(ctx3b, "msg3b")
+	log.Warn(ctx3a, "msg3a", nil)
+	log.Warn(ctx3b, "msg3b", nil)
 
 	testutil.RequireGoldenBytes(t, buf.Bytes())
 }
@@ -58,7 +58,7 @@ func TestErrorWrap(t *testing.T) {
 	err3 := errors.Wrap(err2, "third", z.F64("3", 3))
 
 	ctx := context.Background()
-	log.Error(ctx, "err1", err1)
+	log.Warn(ctx, "err1", err1)
 	log.Error(ctx, "err2", err2)
 	log.Error(ctx, "err3", err3)
 

app/log/testdata/TestErrorWrap.golden
@@ -1,4 +1,4 @@
-00:00 [31mERRO[0m err1: first {"1": 1, "caller": "log_test.go:61"}
+00:00 [33mWARN[0m err1: first {"1": 1, "caller": "log_test.go:61"}
 	app/log/log_test.go:56 .TestErrorWrap
 00:00 [31mERRO[0m err2: second: first {"2": 2, "1": 1, "caller": "log_test.go:62"}
 	app/log/log_test.go:56 .TestErrorWrap

app/retry/retry.go
@@ -33,7 +33,6 @@ import (
 	"github.com/obolnetwork/charon/app/errors"
 	"github.com/obolnetwork/charon/app/log"
 	"github.com/obolnetwork/charon/app/tracer"
-	"github.com/obolnetwork/charon/app/z"
 )
 
 // lateFactor defines the number of slots duties may be late.
@@ -152,7 +151,7 @@ func (r *Retryer) DoAsync(parent context.Context, slot int64, name string, fn fu
 		}
 
 		if ctx.Err() == nil {
-			log.Warn(ctx, "Temporary failure (will retry) calling "+name, z.Err(err))
+			log.Warn(ctx, "Temporary failure (will retry) calling "+name, err)
 			span.AddEvent("retry.backoff.start")
 			select {
 			case <-backoffFunc():

core/leadercast/leadercast.go
@@ -58,23 +58,23 @@ func (l *LeaderCast) Run(ctx context.Context) error {
 		if errors.Is(err, context.Canceled) && ctx.Err() != nil {
 			return nil
 		} else if err != nil {
-			log.Error(ctx, "await next leader duty", err)
+			log.Error(ctx, "Await next leader duty", err)
 			continue
 		}
 
 		if !isLeader(source, l.peers, duty) {
-			log.Warn(ctx, "received duty from non-leader", z.Int("peer", source))
+			log.Warn(ctx, "Received duty from non-leader", nil, z.Int("peer", source))
 			continue
 		}
 
-		log.Debug(ctx, "received duty from leader", z.Int("peer", source), z.Any("duty", duty))
+		log.Debug(ctx, "Received duty from leader", z.Int("peer", source), z.Any("duty", duty))
 
 		var span trace.Span
 		ctx, span = core.StartDutyTrace(ctx, duty, "core/leadercast.Handle")
 
 		for _, sub := range l.subs {
 			if err := sub(ctx, duty, data); err != nil {
-				log.Error(ctx, "subscriber error", err)
+				log.Error(ctx, "Subscriber error", err)
 				continue
 			}
 		}

core/leadercast/transport.go
@@ -69,13 +69,13 @@ func (t *p2pTransport) handle(s network.Stream) {
 	var msg p2pMsg
 	err := json.NewDecoder(s).Decode(&msg)
 	if err != nil {
-		log.Error(ctx, "decode leadercast message", err)
+		log.Error(ctx, "Decode leadercast message", err)
 		return
 	}
 	select {
 	case t.ch <- msg:
 	case <-ctx.Done():
-		log.Warn(ctx, "leadercast transport buffer full")
+		log.Warn(ctx, "Leadercast transport buffer full", nil)
 	}
 }
 
@@ -104,11 +104,11 @@ func (t *p2pTransport) Broadcast(ctx context.Context, fromIdx int, d core.Duty, 
 	}
 
 	if len(errs) == 0 {
-		log.Debug(ctx, "leader broadcast duty success", z.Any("duty", d))
+		log.Debug(ctx, "Leader broadcast duty success", z.Any("duty", d))
 	} else {
 		// len(t.peers)-len(errs)-1 is total number of errors excluding broadcast to self case
-		log.Warn(ctx, "broadcast duty with errors", z.Int("success", len(t.peers)-len(errs)-1),
-			z.Int("errors", len(errs)), z.Str("err_0", errs[0].Error()))
+		log.Warn(ctx, "Broadcast duty with errors", errs[0], z.Int("success", len(t.peers)-len(errs)-1),
+			z.Int("errors", len(errs)))
 	}
 
 	return nil

commit 24b73033d5e9e9be9512869d44a9b79dd9a5b930

app/log: fix counter (#472)
Fixes copy-paste bug introduced in #468.

category: fixbuild
ticket: none

app/log/log.go
@@ -92,11 +92,12 @@ func Info(ctx context.Context, msg string, fields ...z.Field) {
 // Nil err is supported and results in similar behaviour to Info, just at Warn level.
 // Warn should only be used when a problem is encountered that *does not* require any action to be taken.
 func Warn(ctx context.Context, msg string, err error, fields ...z.Field) {
+	incWarnCounter(ctx)
+
 	if err == nil {
 		zfl := unwrapDedup(ctx, fields...)
 		trace.SpanFromContext(ctx).AddEvent("log.Warn: "+msg, toAttributes(zfl))
 		logger.Warn(msg, zfl...)
-		incErrorCounter(ctx)
 
 		return
 	}
@@ -105,18 +106,18 @@ func Warn(ctx context.Context, msg string, err error, fields ...z.Field) {
 	zfl := unwrapDedup(ctx, errFields(err))
 	trace.SpanFromContext(ctx).RecordError(err, trace.WithStackTrace(true), toAttributes(zfl))
 	logger.Warn(err.Error(), zfl...)
-	incErrorCounter(ctx)
 }
 
 // Error wraps err with msg and fields and logs it (incl fields in the context) at Error level.
 // Nil err is supported and results in similar behaviour to Info, just at Error level.
 // Error should only be used when a problem is encountered that *does* require action to be taken.
 func Error(ctx context.Context, msg string, err error, fields ...z.Field) {
+	incErrorCounter(ctx)
+
 	if err == nil {
 		zfl := unwrapDedup(ctx, fields...)
 		trace.SpanFromContext(ctx).AddEvent("log.Error: "+msg, toAttributes(zfl))
 		logger.Error(msg, zfl...)
-		incErrorCounter(ctx)
 
 		return
 	}
@@ -125,7 +126,6 @@ func Error(ctx context.Context, msg string, err error, fields ...z.Field) {
 	zfl := unwrapDedup(ctx, errFields(err))
 	trace.SpanFromContext(ctx).RecordError(err, trace.WithStackTrace(true), toAttributes(zfl))
 	logger.Error(err.Error(), zfl...)
-	incErrorCounter(ctx)
 }
 
 // unwrapDedup returns the wrapped zap fields from the slice and from the context. Duplicate fields are dropped.